7faab182994708192f6fd428b75de07f02e74e71
paulmark15 <pavel.markevic1@gmail.com>
2016-11-11 19:54:26 +0200

    team responsive

--- resources/views/frontend/pages/contacts.blade.php
+++ resources/views/frontend/pages/contacts.blade.php
@@ -2,16 +2,17 @@
 @section('page-title', trans('seo.contacts.title'))
 @section('meta-description', trans('seo.contacts.description'))
 @section('nav-style', 'nav-authorize-question')
+@section('body-class', 'contacts-page')
 @section('content')
     <section class="page-title page-title-4 image-bg parallax">
-        <div class="background-image-holder fadeIn">
-            <img alt="Background Image" class="background-image" src="{{ url()->to('/') }}/images/cover16.jpg" />
+        <div class="background-image-holder-about fadeIn">
+            <!--img alt="Background Image" class="background-image" src="{{ url()->to('/') }}/images/cover16.jpg" /-->
         </div>
-        <div class="container">
+        <div class="container page-first-header">
             <div class="row">
                 <div class="col-md-6">
-                    <h2 class="uppercase mb8">Contact us</h2>
-                    <p class="lead mb0">We are always happy to hear from you.</p>
+                    <h1 class="uppercase mb8 page-h2">Contact us</h1>
+                    <h2 class="lead mb0 below">Feel free to <span class="color-green">contact us.</span></h2>
                 </div>
             </div>
             <!--end of row-->
@@ -19,7 +20,99 @@
         <!--end of container-->
     </section>
     <section>
-        <div class="container">
+
+
+        <div class="arrow-style index3 mob-left-to-right">
+            <div class="curve-wrap left-top-wrap">
+                <div class="rotated left-top">
+                    <div class="top-part"></div>
+                </div>
+            </div>
+            <div class="curve-wrap right-top-wrap">
+                <div class="rotated right-top">
+                    <div class="top-part"></div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="half left about-block">
+                            <h3 class="uppercase">Get In Touch</h3>
+                            <p>Have something to share with us: a piece of advice, feedback, or a problem? Just fill out our form and we will get back to you shortly with the answer that you have been looking for. And trust me, we are quick!</p>
+                            <hr>
+                            <p>
+                                <strong>Pixsens LTD</strong>
+                                <br /> Kemp House
+                                <br /> 160 City Road
+                                <br /> London EC1V 2NX
+                            </p>
+                            <hr>
+                            <p>
+                                <strong>E:</strong> <span id="eadr">m<b>@</b>e@d<b>no</b>oma<b>.com</b>in.com</span>
+                                <br />
+                                {{--<strong>P:</strong> +614 3948 2726
+                                <br />--}}
+                            </p>
+                    </div>
+                    <div class="half right about-block referral">
+                        @if (Session::has('flash_notification.general.message'))
+                            <div class="alert alert-{{ Session::get('flash_notification.general.level') }} alert-dismissible" role="alert">
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                    <span aria-hidden="true">×</span>
+                                </button>
+                                {{ Session::get('flash_notification.general.message') }}
+                            </div>
+                        @endif
+                        @if (count($errors->contacts) > 0)
+                            <div class="alert alert-danger alert-dismissible" role="alert">
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                    <span aria-hidden="true">×</span>
+                                </button>
+                                <ul>
+                                    @foreach ($errors->contacts->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+                        {!! Form::open([
+                            'role' => 'form',
+                            'url' => action('FrontendController@contactForm'),
+                            'class' => 'form-contacts',
+                            'method' => 'POST'
+                        ]) !!}
+                        <div class="hidden">
+                            {!! Form::text('birthday', null) !!}
+                            {!! Form::text('city', 'London') !!}
+                        </div>
+                        {!! Form::text('name', null, ['placeholder' => 'Your Name']) !!}
+                        {!! Form::text('email', null, ['placeholder' => 'Email Address']) !!}
+                        {!! Form::textarea('message', null, ['rows' => 4, 'placeholder' => 'Message']) !!}
+                        <button type="submit">Send Message</button>
+                        {!! Form::close() !!}
+                    </div>
+                    <div class="clearboth"></div>
+                </div>
+            </div>
+
+            <div class="curve-wrap left-bottom-wrap">
+                <div class="rotated left-bottom">
+                    <div class="bottom-part"></div>
+                </div>
+            </div>
+            <div class="curve-wrap right-bottom-wrap">
+                <div class="rotated right-bottom">
+                    <div class="bottom-part"></div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+
+        <!--div class="container">
             <div class="row">
                 <div class="col-sm-6 col-md-5">
                     <h4 class="uppercase">Get In Touch</h4>
@@ -76,9 +169,9 @@
                         <button type="submit">Send Message</button>
                     {!! Form::close() !!}
                 </div>
-            </div>
+            </div-->
             <!--end of row-->
-        </div>
+        <!--/div-->
         <!--end of container-->
     </section>
     @include('frontend/footer')
--- resources/views/frontend/pages/team.blade.php
+++ resources/views/frontend/pages/team.blade.php
@@ -2,16 +2,17 @@
 @section('page-title', trans('seo.team.title'))
 @section('meta-description', trans('seo.team.description'))
 @section('nav-style', 'nav-authorize-question')
+@section('body-class', 'team-page')
 @section('content')
     <section class="page-title page-title-4 image-bg parallax">
-        <div class="background-image-holder fadeIn">
-            <img alt="Background Image" class="background-image" src="{{ url()->to('/') }}/images/cover5.jpg" />
+        <div class="background-image-holder-about fadeIn">
+            <!--img alt="Background Image" class="background-image" src="{{ url()->to('/') }}/images/cover5.jpg" /-->
         </div>
-        <div class="container">
+        <div class="container page-first-header">
             <div class="row">
                 <div class="col-md-6">
-                    <h2 class="uppercase mb8">The team</h2>
-                    <p class="lead mb0">Teamwork makes the dream work.</p>
+                    <h1 class="uppercase mb8 page-h2">The team</h1>
+                    <h2 class="lead mb0 below"><span class="color-pink">Teamwork</span> makes the dream work.</h2>
                 </div>
             </div>
             <!--end of row-->
@@ -19,28 +20,124 @@
         <!--end of container-->
     </section>
     <section>
-        <div class="container">
-            <div>
-                <h4>The Fashion-Forward Team of Stylesensei</h4>
-                <p>Stylesensei is run by a group of highly talented fashion-forward individuals who have the unique creativity needed to style you on a daily basis. Our team consists of individuals who are true fashion fanatics, and keep tabs on the latest trends, fashion icons, and styling ideas to forward them to our customers. Whether you want to dress up to impress your new boss at a job interview or simply want to head out to a party with friends, we have got you covered.</p>
-                <p>Ask us anything about fashion and styling, and we will answer giving you the best advice to look and feel great. Our style advice is meant to make you stand out in the crowd and look effortlessly chic and on-point.</p>
+
+
+        <!--  FIRST CONTAINER   -->
+
+        <div class="arrow-style mob-left-to-right nopad800">
+            <div class="curve-wrap left-top-wrap">
+                <div class="rotated left-top">
+                    <div class="top-part"></div>
+                </div>
+            </div>
+            <div class="curve-wrap right-top-wrap">
+                <div class="rotated right-top">
+                    <div class="top-part"></div>
+                </div>
+            </div>
+            <div class="container first-about-block">
+                <div class="row index3">
+                    <div class="half left about-block mobile-absolute position1 zindex-1">
+                    </div>
+                    <div class="half right nofloat-mob about-block team-first-text mt56 mb48">
+                        <h3 class="uppercase mb0">The<span class="br"></span> Fashion-Forward</h3>
+                        <h4 class="uppercase padright60on410">Team of <span class="color-pink">Stylesensei</span></h4>
+                        <p>Stylesensei is run by a group of highly talented fashion-forward individuals who have the unique creativity needed to style you on a daily basis. Our team consists of individuals who are true fashion fanatics, and keep tabs on the latest trends, fashion icons, and styling ideas to forward them to our customers.</p>
+                        <p>Whether you want to dress up to impress your new boss at a job interview or simply want to head out to a party with friends, <span class="color-pink bold700">we have got you covered.</span></p>
+                    </div>
+                </div>
+                <div class="clearboth"></div>
+
             </div>
 
-            <div>
-                <h4>Get on Board with Trendy Individuals</h4>
-                <p>Are you fond of styling others and giving them dressing advice? Do you enjoy dressing up others? Do you get asked about your styling sense? If yes, why not join our style-experts’ team? At Stylesensei, we are always on the lookout of stylish and fashionable individuals who can make a difference in other people’s sense of dressing. We want to get positive, creative, and fun people on board with us who can help others look fabulous all day, every day.</p>
-                <p>So, if you think you have it in you, join our team by sending us your CV. Make sure you tell us your story, why you want to be a part of Stylesensei, and what’s special about you to get shortlisted.</p>
+            <div class="curve-wrap left-bottom-wrap">
+                <div class="rotated left-bottom">
+                    <div class="bottom-part"></div>
+                    <img class="curved-image-rmw red-pos1" alt="Stylish man" src="{{ url()->to('/') }}/images/red-man.png">
+                    <img class="curved-image-rmw red-pos2" alt="Stylish woman" src="{{ url()->to('/') }}/images/red-woman.png">
+                </div>
             </div>
-
-            <div>
-                <h4>Make a Move Now!</h4>
-                <p>At Stylesensei, we are always searching for voguish individuals to be a part of our team. Our platform will help individuals get better styling and dressing advice, which is why we need experienced individuals to help us boost others’ confidence in their style and overall look.</p>
-            </div>
-
-            <div>
-                <h5>Get your style sorted out by getting in touch with our style-experts!</h5>
+            <div class="curve-wrap right-bottom-wrap">
+                <div class="rotated right-bottom">
+                    <div class="bottom-part"></div>
+                    <img class="mobile-red-img mob-red-pos1 absolute visible990" alt="Stylish man" src="{{ url()->to('/') }}/images/red-man.png">
+                    <img class="mobile-red-img mob-red-pos2 absolute visible990" alt="Stylish woman" src="{{ url()->to('/') }}/images/red-woman.png">
+                </div>
             </div>
         </div>
+
+             <!--  FASHION AND STYLING   -->
+
+        <div class="arrow-style-invert">
+
+            <div class="container container1400">
+                <div class="half left about-block on-black blog ask-us">
+                    <h3 class="uppercase blog-head relative"><span class="fs44">Ask us anything about</span> <span class="color-blue">fashion and styling</span><img class="absolute fashion-icon" alt="Blog icon" src="{{ url()->to('/') }}/images/blog-blue-icon.svg"></h3>
+                    <p><span class="bold700">We will answer giving you the best advice to look and feel great.</span><br> Our style advice is meant to make you stand out in the crowd and look effortlessly chic and on-point.</p>
+                </div>
+            </div>
+
+            <img class="right-image-team" alt="Stylish computer" src="{{ url()->to('/') }}/images/manikeny.png" >
+
+        </div>
+
+
+        <!--  GET ON BOARD   -->
+
+
+
+
+        <div class="arrow-style mob-right-to-left">
+            <div class="curve-wrap left-top-wrap">
+                <div class="rotated left-top">
+                    <div class="top-part"></div>
+                    <img class="absolute position4" alt="Stylish people lying" src="{{ url()->to('/') }}/images/trendy-individuals.jpg">
+                </div>
+            </div>
+            <div class="curve-wrap right-top-wrap">
+                <div class="rotated right-top">
+                    <div class="top-part"></div>
+                    <img class="absolute position6 visible990" alt="Stylish people lying" src="{{ url()->to('/') }}/images/trendy-individuals.jpg">
+                </div>
+            </div>
+            <div class="container get-on-board">
+                <div class="row index3 mb32">
+                    <div class="half left about-block">
+                    </div>
+                    <div class="half right nofloat-mob about-block pl30">
+                        <h3 class="uppercase padright60on410 mb0">Get on board</h3>
+                        <h4 class="uppercase color-cyan">With trendy individuals</h4>
+                        <p>Are you fond of styling others and giving them dressing advice? Do you enjoy dressing up others? Do you get asked about your styling sense? If yes, why not join our style-experts’ team? At Stylesensei, we are always on the lookout of stylish and fashionable individuals who can make a difference in other people’s sense of dressing. We want to get positive, creative, and fun people on board with us who can help others look fabulous all day, every day.</p>
+                        <p>So, if you think you have it in you, join our team by sending us your CV. Make sure you tell us your story, why you want to be a part of Stylesensei, and what’s special about you to get shortlisted.</p>
+                    </div>
+                </div>
+                <div class="row index3">
+                    <div class="half left about-block text-right pr30">
+                        <h3 class="uppercase mb0">Make a move now!</h3>
+                        <h4 class="uppercase color-cyan">We are searching for you!</h4>
+                        <p>At Stylesensei, we are always searching for voguish individuals to be a part of our team. Our platform will help individuals get better styling and dressing advice, which is why we need experienced individuals to help us boost others’ confidence in their style and overall look. Get your style sorted out by getting in touch with our style-experts!</p>
+                    </div>
+                    <div class="half right about-block">
+                    </div>
+                </div>
+                <div class="clearboth"></div>
+
+            </div>
+
+            <div class="curve-wrap left-bottom-wrap">
+                <div class="rotated left-bottom">
+                    <div class="bottom-part"></div>
+                    <img class="absolute position7 visible990" alt="Stylish clothes" src="{{ url()->to('/') }}/images/make-move-now.jpg">
+                </div>
+            </div>
+            <div class="curve-wrap right-bottom-wrap">
+                <div class="rotated right-bottom">
+                    <div class="bottom-part"></div>
+                    <img class="curved-image-rb position5" alt="Stylish clothes" src="{{ url()->to('/') }}/images/make-move-now.jpg">
+                </div>
+            </div>
+        </div>
+
     </section>
     @include('frontend/footer')
 @stop
