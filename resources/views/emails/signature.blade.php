c7a63cbeec779e055a2459ada327461911cd0e62
Stanislav <stanislav@pixsens.co.uk>
2016-12-12 21:01:25 +0000

    fixed sgnature

--- resources/views/emails/signature.blade.php
+++ resources/views/emails/signature.blade.php
@@ -4,7 +4,7 @@
         <td colspan="2" class="logo-td" align="left" valign="top" width="200">
             <p style="margin-top: 0; margin-right: 10px; font-family: Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; margin-bottom: 0;">
                 <a style="text-decoration:none" href="https://www.stylesensei.co.uk" class="clink logo-container">
-                    <img moz-do-not-send="true" src="https://www.stylesensei.co.uk/images/email-logo.png" alt="Style Sensei" border="0" class="sig-logo" height="18" width="146">
+                    <img moz-do-not-send="true" src="https://www.stylesensei.co.uk/images/email-logo.png" alt="Style Sensei" border="0" class="sig-logo" style="width: 146px; height: 18px;" height="18" width="146">
                 </a>
             </p>
         </td>
@@ -13,19 +13,19 @@
         <td colspan="2">
             <p class="social-list" style="font-size: 0px; line-height: 0; font-family: Helvetica, Arial, sans-serif; margin-top: 10px;">
                 <a style="text-decoration: none; display: inline; margin-right: 20px;" class="social signature_twitter-target sig-hide" href="{{ env('FACEBOOK_URL') }}">
-                    <img moz-do-not-send="true" width="18" style="margin-bottom:2px; border:none; display:inline;" height="18" data-filename="twitter.png" src="https://www.stylesensei.co.uk/images/social-1_logo-facebook.png" alt="Twitter">
+                    <img moz-do-not-send="true" width="18" style="margin-bottom:2px; border:none; display:inline; width: 18px; height: 18px;" height="18" data-filename="twitter.png" src="https://www.stylesensei.co.uk/images/social-1_logo-facebook.png" alt="Twitter">
                 </a>
 
                 <a style="text-decoration: none; display: inline; margin-right: 20px;" class="social signature_facebook-target sig-hide" href="{{ env('INSTAGRAM_URL') }}">
-                    <img moz-do-not-send="true" width="18" style="margin-bottom:2px; border:none; display:inline;" height="18" data-filename="facebook.png" src="https://www.stylesensei.co.uk/images/social-1_logo-instagram.png" alt="Facebook">
+                    <img moz-do-not-send="true" width="18" style="margin-bottom:2px; border:none; display:inline; width: 18px; height: 18px;" height="18" data-filename="facebook.png" src="https://www.stylesensei.co.uk/images/social-1_logo-instagram.png" alt="Facebook">
                 </a>
 
                 <a style="text-decoration: none; display: inline; margin-right: 20px;" class="social signature_googleplus-target sig-hide" href="{{ env('TWITTER_URL') }}">
-                    <img moz-do-not-send="true" width="18" style="margin-bottom:2px; border:none; display:inline;" height="18" data-filename="googleplus.png" src="https://www.stylesensei.co.uk/images/social-1_logo-twitter.png" alt="Google +">
+                    <img moz-do-not-send="true" width="18" style="margin-bottom:2px; border:none; display:inline; width: 18px; height: 18px;" height="18" data-filename="googleplus.png" src="https://www.stylesensei.co.uk/images/social-1_logo-twitter.png" alt="Google +">
                 </a>
 
                 <a style="text-decoration: none; display: inline; margin-right: 10px;" class="social signature_linkedin-target sig-hide" href="{{ env('GOOGLE_URL') }}">
-                    <img moz-do-not-send="true" width="18" style="margin-bottom:2px; border:none; display:inline;" height="18" data-filename="linkedin.png" src="https://www.stylesensei.co.uk/images/social-1_logo-google-plus.png" alt="LinkedIn">
+                    <img moz-do-not-send="true" width="18" style="margin-bottom:2px; border:none; display:inline; width: 18px; height: 18px;" height="18" data-filename="linkedin.png" src="https://www.stylesensei.co.uk/images/social-1_logo-google-plus.png" alt="LinkedIn">
                 </a>
             </p>
         </td>
