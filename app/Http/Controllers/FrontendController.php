0b6e29d1d499e850e6d6baca0de9ff9fbc737c86
Stanislav Markevic <stanislav.markevic@gmail.com>
2019-03-18 19:38:30 +0000

    added antilope

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -43,7 +43,7 @@ class FrontendController extends Controller
             return $page;
         }*/
         $phrase = Phrases::where(['enabled' => 1])->inRandomOrder()->first();
-        $backgrounds = ['back4', 'antilopa3'];
+        $backgrounds = ['back4', 'antilope'];
 
         $view = view('frontend/pages/index')->with([
             'background' => $backgrounds[array_rand($backgrounds)],
