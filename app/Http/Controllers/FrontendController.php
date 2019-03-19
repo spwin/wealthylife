24153f55eb30ae5decfef6cf4798cdd84725f462
Stanislav Markevic <stanislav.markevic@gmail.com>
2019-03-19 02:52:24 +0000

    added leopard

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -43,7 +43,7 @@ class FrontendController extends Controller
             return $page;
         }*/
         $phrase = Phrases::where(['enabled' => 1])->inRandomOrder()->first();
-        $backgrounds = ['back4', 'antilope'];
+        $backgrounds = ['back4', 'antilope', 'leopard.jpg'];
 
         $view = view('frontend/pages/index')->with([
             'background' => $backgrounds[array_rand($backgrounds)],
