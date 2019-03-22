44fa3ae54beb2727ad387d5f8004e88e71a1b186
Stanislav Markevic <stanislav.markevic@gmail.com>
2019-03-22 21:22:09 +0000

    added owieczka

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -43,7 +43,7 @@ class FrontendController extends Controller
             return $page;
         }*/
         $phrase = Phrases::where(['enabled' => 1])->inRandomOrder()->first();
-        $backgrounds = ['back4', 'antilope', 'leopard'];
+        $backgrounds = ['back4', 'antilope', 'leopard', 'owieczka'];
 
         $view = view('frontend/pages/index')->with([
             'background' => $backgrounds[array_rand($backgrounds)],
